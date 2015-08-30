aa8379b0ac4ed31b8ecb961b145a6a569be68283
ceditoph <CR.patterson2000@gmail.com>
2015-08-30 16:09:35 -0700

    refactored to old request

--- Mobile/server.js
+++ Mobile/server.js
@@ -1,33 +1,33 @@
 var app = require('express')();
-var rp = require('request-promise');
+var $http = require('request');
 
-app.get('/', function(req, res){
+ app.get('/', function(req, res){
   res.send('hello world');
-});
+ });
 //*******currently this is handling all requests to all paths************\\
 //it will need to be changed to handle the different types of requests.
-app.all('*', function(req, res){
+ app.all('*', function(req, res){
   res.write ('you\'ve been Katfished!');
   var userID = req.url.toString().match(/[0-9]+/g)[0];
   console.log('Katifish engaged on ', userID);
   var result = getData(userID);
+  console.log(result);
 });
 
 var getData = function(queryParam){
-  var options = {
-    uri : 'https://incandescent-inferno-4780.firebaseio.com/pond.json.'+queryParam,
-    method : 'GET'
-  }
+  var queryParam = queryParam;
   var traits = [];
-  rp(options)
-  .then(function(response){
-    var user = JSON.parse(body);
-    for(var key in user[queryParam]){
-      if(key !== 'name') traits.push([user[queryParam][key].length, key]);
-    };
-  })
-  console.log(traits.sort().reverse());
+  $http.get('https://incandescent-inferno-4780.firebaseio.com/pond.json', function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      var user = JSON.parse(body);
+      for(var key in user[queryParam]){
+       if(key !== 'name') traits.push([user[queryParam][key].length, key]);
+      };
+      traits.sort().reverse();
+    }
+    console.log(traits);
+  });
 }
 
 app.listen(3000);
-console.log( 'Server is listening on 3000' )
+console.log( 'Server is running on 3000' )
