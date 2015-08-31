0f891e31921571a30809c3109505bbcd4c2e24a2
ceditoph <CR.patterson2000@gmail.com>
2015-08-31 12:25:26 -0700

    styles exported

--- Mobile/index.ios.js
+++ Mobile/index.ios.js
@@ -5,9 +5,10 @@
  */
  'use strict';
 
- var React = require('react-native');
- var Firebase = require('firebase');
- var d3 = require('d3');
+var React = require('react-native');
+var Firebase = require('firebase');
+var styles = require('./styles').style;
+var d3 = require('d3');
 
  var {
   AppRegistry,
@@ -155,53 +156,5 @@ showAlert() {
 
 });
 
-var styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-  rightContainer: {
-    flex: 1,
-    backgroundColor: "#3399FF"
-  },
-
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
-  },
-
-  button: {
-    height: 44,
-    flexDirection: 'row',
-    backgroundColor: '#48BBEC',
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  }
-});
 
 AppRegistry.registerComponent('ThesisProject', () => ThesisProject);
