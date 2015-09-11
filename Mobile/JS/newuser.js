4dbb562a5f3bc05a7e0333bc5fea2ff56280ecbe
ChristopherSalam <christopher.salam@gmail.com>
2015-09-11 16:21:40 -0700

    Added tests to newuser.js, will start adding them throughout

--- Mobile/JS/newuser.js
+++ Mobile/JS/newuser.js
@@ -3,39 +3,69 @@
 ||   logs in and doesn't already have a match in our db   ||
 ||========================================================*/
 
-/* assumed input from FB: {Kim Merino, 2529521} */
-var newuser = {"name": "Casey Patterson", "id": "119553731730477"}
-
-var Firebase = require('firebase'),
+var test = require('tape'),
+  newuser = {"name": "Test User", "id": "99999"},
+  Firebase = require('firebase'),
   ref = new Firebase("https://katfish.firebaseio.com/"),
-  pondRef = ref.child("pond");
+  pondRef = ref.child("pond"),
+  breyRef = pondRef.child("714387395"),
+  breyBallerRef = breyRef.child("baller"),
+  dbGallery = [];
 
 var qualities = ["baller","leader","performer","teacher","romantic","analytical","brave","counseling","confident","creative","dynamic","driven","extroverted","flirty","mysterious","grounded","artsy","dreamer","funny","smart","careful","calm","decisive","reliable","thoughtful","loyal","sincere","versatile","understanding","independent","honest","kind"];
-  // randomIDmaker = function(index) { return Object.keys(index)[Math.floor(Math.random()*Object.keys(index).length)]; };
 
-pondRef.on("value", function (snapshot) {
-  var index = snapshot.val();
-  //albrey test
-  console.log(index["714387395"].name +" is a user: "+ Boolean(Object.keys(index).indexOf("714387395") >= 0))
-  console.log("---------------------------");
-  //kim test
-  console.log(newuser.name +" is a user: "+ Boolean(Object.keys(index).indexOf(newuser.id) >= 0));
-  console.log("---------------------------");
+test('database exists', function(t){
+  t.ok(pondRef, 'DB connection works');
+  t.equal(pondRef.key(),"pond");
+  t.end();
 });
 
+test('brey exists', function(t){
+  t.ok(breyRef, 'Brey connection works');
+  t.equal(breyRef.key(),"714387395");
+  t.end();
+});
+
+test('brey as a baller exists', function(t){
+  t.ok(breyBallerRef, 'baller connection works');
+  t.equal(breyBallerRef.key(),"baller");
+  t.end();
+});
+
+/*========================================================||
+||   Here we will run a setting make a new user           ||
+||   logs in and doesn't already have a match in our db   ||
+||========================================================*/
+
 var newuserRef = pondRef.child(newuser.id);
-  for (var i = 0; i < qualities.length; i++) {
-    newuser[qualities[i]] = {};
-    newuser[qualities[i]][newuser.id] = true;
+
+for (var i = 0; i < qualities.length; i++) {
+  newuser[qualities[i]] = {};
+  newuser[qualities[i]][newuser.id] = true;
+}
+
+newuserRef.set(newuser, function(error) {
+  if (error) {
+    console.log("newuser name & ID could not be saved." + error);
+  } else {
+    console.log("newuser name & ID saved successfully.");
+    newuserRef.remove();
+    console.log("newuser name & ID removed successfully.");
   }
-  newuserRef.set(newuser, function(error) {
-    if (error) {
-      console.log("newuser name & ID could not be saved." + error);
-    } else {
-      console.log("newuser name & ID saved successfully.");
-    }
+});
 
-  });
+/*========================================================||
+||   Database testing is asychhronous with Firebase       ||
+||   This listener watches for updates and logs           ||
+||========================================================*/
 
-  // newuserRef.remove();
+pondRef.on("value", function (snapshot) {
+  console.log("# asychronous tests --------")
+  console.log("DB changed to", Object.keys(snapshot.val()).length+" people");
+});
+
+
+
+
+
 
